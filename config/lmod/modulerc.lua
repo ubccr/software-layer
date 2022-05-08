bb383eb29fccb4365eba6ed33f4da06ba087eb21
Andrew E. Bruno <aebruno2@buffalo.edu>
2022-05-07 21:10:56 -0400

    Hide intel-compilers

--- config/lmod/modulerc.lua
+++ config/lmod/modulerc.lua
@@ -8,9 +8,11 @@ module_version("gcc/9.3.0","9")
 module_version("gcc/10.3.0","10")
 module_version("gcc/11.2.0","11")
 module_version("intel/2020a","2020")
+module_version("intel/2021a","2021")
 
 -- hidden modules
 hide_version("gcccore/9.3.0")
 hide_version("gcccore/10.3.0")
 hide_version("gcccore/11.2.0")
 hide_version("iccifort/2020.1.217")
+hide_version("intel-compilers/2021.2.0")
