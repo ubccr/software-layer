77c102185db7ce7427aaa4bce49b9ec448d90e85
Andrew E. Bruno <aebruno2@buffalo.edu>
2022-05-24 22:27:18 -0400

    Only run for uid > 1000

--- config/profile/bash.sh
+++ config/profile/bash.sh
@@ -1,9 +1,10 @@
 CCR_INIT_DIR=$(dirname "$(dirname "$(readlink -f "$BASH_SOURCE")")")
-
 export CCR_INIT_DIR
 
-for file in ${CCR_INIT_DIR}/profile/profile.d/*.sh; do
-if [[ -r "$file" ]]; then
-    source $file
+if [[ $UID -ge 1000 ]]; then
+    for file in ${CCR_INIT_DIR}/profile/profile.d/*.sh; do
+        if [[ -r "$file" ]]; then
+            source $file
+        fi
+    done
 fi
-done
