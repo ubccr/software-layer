e3c73b6448c673aba0973d8d5dbd6fd192f1ab0f
Andrew E. Bruno <aebruno2@buffalo.edu>
2022-11-30 15:18:37 -0500

    Fix module configs

--- config/lmod/modulerc.lua
+++ config/lmod/modulerc.lua
@@ -1,16 +1,15 @@
 -- CCR default versions
 module_version("StdEnv/2023.01","default")
-module_version("easybuild/4.6.0","default")
-module_version("gcc/9.3.0","default")
-module_version("gcccore/9.3.0","default")
-module_version("intel/2020a","default")
+module_version("easybuild/4.6.2","default")
+module_version("gcc/11.2.0","default")
+module_version("gcccore/11.2.0","default")
+module_version("intel/2022.00","default")
 
 -- shortcuts
 module_version("gcc/9.3.0","9")
 module_version("gcc/10.3.0","10")
 module_version("gcc/11.2.0","11")
-module_version("intel/2020a","2020")
-module_version("intel/2021a","2021")
+module_version("intel/2022.00","2022")
 
 -- hidden modules
 hide_version("easybuild/4.5.4")
