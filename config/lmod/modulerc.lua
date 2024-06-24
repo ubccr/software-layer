fed1da90f5a43162397f54d9962a62b39a46e5fc
Andrew E. Bruno <aebruno2@buffalo.edu>
2024-06-24 16:20:46 -0400

    Remove old version

--- config/lmod/modulerc.lua
+++ config/lmod/modulerc.lua
@@ -2,11 +2,9 @@
 module_version("pavilion/2.4.7217d1f","default")
 
 -- hidden modules
-hide_version("ccrsoft/2022.05")
 hide_version("ccrarch/avx")
 hide_version("ccrarch/avx2")
 hide_version("ccrarch/avx512")
 hide_version("ccrenv")
-hide_version("gentoo/2022.05")
 hide_version("gentoo/2023.01")
 hide_version("gentoo/2024.04")
