55203cf2ef08d776519fa66c9d5197ee0ddb50fc
Andrew E. Bruno <aebruno2@buffalo.edu>
2024-12-10 15:48:45 -0500

    Bump default easybuild version to 4.9.4
    Signed-off-by: Andrew E. Bruno <aebruno2@buffalo.edu>

--- config/lmod/modulerc_2024.04.lua
+++ config/lmod/modulerc_2024.04.lua
@@ -1,6 +1,6 @@
 -- CCR default versions
 module_version("ccrsoft/2024.04","default")
-module_version("easybuild/4.9.1","default")
+module_version("easybuild/4.9.4","default")
 
 -- shortcuts
 module_version("gcc/13.2.0","13")
@@ -10,3 +10,5 @@ module_version("gcc/13.2.0","13")
 if os.getenv("CCR_CPU_FAMILY") == "aarch64" then
     hide_version("ccrsoft/2023.01")
 end
+
+hide_version("easybuild/4.9.1")
